24fbce7670559e95f527c5a67fe4ef0c1cae6f0d
Ruslan Xusenov <kali@Ruslan.kali>
2024-12-17 12:02:46 +0500

    Github uy ishi

--- 2-misol.py
+++ 2-misol.py
@@ -1,4 +1,13 @@
 from os import *
 system("clear")
 
-def 
+def letter(s):
+    if s:
+        return s[0].upper() + s[1:]
+    return s
+
+print(letter("Hello"))
+print(letter("hello"))
+print(letter("a"))
+print(letter(" "))
+print(letter("1q"))
