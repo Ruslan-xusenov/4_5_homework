5251d49f3b521ccc0b61463a92458a23dea629af
Ruslan Xusenov <kali@Ruslan.kali>
2024-12-17 12:24:14 +0500

    Welcome to the City

--- 4-misol.py
+++ 4-misol.py
@@ -1,4 +1,8 @@
 from os import *
 system("clear")
 
-def 
+def shahar_ism(ism, shahar, davlat):
+    toliq_ism = " ".join(ism)
+    return f"Salom {toliq_ism}! {shahar}ning {davlat} shahriga xush kelibsiz !!!"
+
+print(shahar_ism(["Ruslan", "Xusenov"], "O'zbekiston", "Toshkent"))
