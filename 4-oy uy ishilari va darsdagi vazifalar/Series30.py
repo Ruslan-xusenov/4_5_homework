ce04f24577ef3c8193a8901ef475d89d556ff090
Ruslan Xusenov <kali@Ruslan.kali>
2024-12-17 11:53:21 +0500

    Github uy ishi

--- 4-oy uy ishilari va darsdagi vazifalar/Series30.py
+++
@@ -1,9 +0,0 @@
-from os import *
-system("clear")
-
-k = int(input("To'plamlar sonini kiriting: "))
-
-for i in range(k):
-    lst = list(map(int, input(f"{i + 1}-to'plam elementlarini kiriting: ").split()))
-    add = sum(lst)
-    print(f"{i + 1}-to'plam yig'indisi:", add)
--- Dars 11/t.py
+++
@@ -1,8 +0,0 @@
-from os import *
-system('clear')
-
-a = 300
-b = 300
-print(a is b)
-print(id(a))
-print(id(b))
